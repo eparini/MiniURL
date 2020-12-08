2d4c5398a73204d31eaf074337060c3e121346a0
Emanuele Parini <emanuele.parini@armonicaretail.com>
2020-12-08 01:17:27 +0100

    --

--- MiniURL/Services/CosmosDbService.cs
+++ MiniURL/Services/CosmosDbService.cs
@@ -12,10 +12,7 @@ namespace MiniURL
     {
         private Container _container;
 
-        public CosmosDbService(
-            CosmosClient dbClient,
-            string databaseName,
-            string containerName)
+        public CosmosDbService(CosmosClient dbClient, string databaseName, string containerName)
         {
             this._container = dbClient.GetContainer(databaseName, containerName);
         }
